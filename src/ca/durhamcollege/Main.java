ad79efc9bb266627c53324b7bb2e2d8103b31455
Tom Tsiliopoulos <tsilipoulos@hotmail.com>
2020-11-11 11:48:17 -0500

    contrived example about multi-dimensional arrays

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -51,11 +51,27 @@ public class Main
         while(!isValidInput);
 
 
+        // multi-dimensional array example
 
+        System.out.println("Multi-Dimensional Array example");
+        int rows = 8;
+        int cols = 8;
+        float[][] board = new float[rows][cols];
 
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < cols; y++) {
+                board[x][y] = (float)(Math.random() * 100.0) + 1.0f;
+            }
+        }
 
+        System.out.println("Showing 10 Random Values: ");
 
-
+        for (int i = 0; i < 10; i++) {
+            var randRows = (int)(Math.random() * rows);
+            var randCols = (int)(Math.random() * cols);
+            System.out.print("For row: " + randRows + " col: " + randCols + " ");
+            System.out.printf("The Random number is: %.2f \n", board[randRows][randCols]);
+        }
 
         // Output
         System.out.println();
